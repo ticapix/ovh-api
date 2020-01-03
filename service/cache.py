1528774ee97f22e4e204620361076e6b59c0f750
Pierre Gronlier <pierre.gronlier@corp.ovh.com>
2020-01-03 14:15:28 +0000

    remove unused var

--- service/cache.py
+++ service/cache.py
@@ -3,12 +3,13 @@ from aiocache import SimpleMemoryCache, MemcachedCache
 import socket
 import os
 
+
 def DetectCache(cache: dict) -> dict:
     cache['cache'] = SimpleMemoryCache
     mem_port = int(os.getenv('MEMCACHED_PORT', '11211'))
     mem_host = os.getenv('MEMCACHED_HOST', '127.0.0.1')
     try:
-        conn = socket.create_connection((mem_host, mem_port), timeout=1)
+        _ = socket.create_connection((mem_host, mem_port), timeout=1)
         cache['cache'] = MemcachedCache
         cache['endpoint'] = mem_host
         cache['port'] = mem_port
