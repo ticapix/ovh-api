a1abaf4f487aa34f5a834d95bbe49bf1d0943d1a
Pierre Gronlier <pierre.gronlier@corp.ovh.com>
2020-01-03 14:15:02 +0000

    mv print statement to logging

--- service/convert.py
+++ service/convert.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-# import json
-# from pathlib import Path
-# import yaml
-# import requests
 
-# from datetime import timedelta
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def gen_model_path(model):
     return '#/components/schemas/{}'.format(model)
 
-def convert_type(type_, model_refs):
+
+def convert_type(type_, model_refs) -> dict:
+    logger.debug("type: {}".format(type_))
     type_mapping = {
         'boolean': ('boolean', ),
         'date': ('string', 'date'),
@@ -23,7 +24,7 @@ def convert_type(type_, model_refs):
         return {
             'type': 'array',
             'items': convert_type(type_[:-2], model_refs)
-            }
+        }
     if type_ in type_mapping:
         ret = {
             'type': type_mapping[type_][0]
@@ -36,10 +37,11 @@ def convert_type(type_, model_refs):
             '$ref': gen_model_path(type_)
         }
     if type_ != 'string':
-        print("WARNING: unknown type '{}'".format(type_))
+        logger.warning("unknown type '{}'".format(type_))
     return {
         'type': 'string'
     }
+
 
 def convert_properties(properties, model_refs):
     for name, property_ in properties.items():
@@ -60,18 +62,22 @@ def convert_model(models, model_refs):
                 'enum': model['enum']
             })
             continue
-        print("ERROR: unknown type '{}'".format(model))        
-        import pdb; pdb.set_trace()
+        logger.error("unknown type '{}'".format(model))
+        if logger.level == logging.DEBUG:
+            import pdb
+            pdb.set_trace()
 
-def convert_parameter(parameters, model_refs):
+
+def convert_parameter(parameters, model_refs) -> dict:
     for parameter in parameters:
         yield {
             'name': parameter['name'],
-            'in': parameter['paramType'], # query, path, header, cookie, 
+            'in': parameter['paramType'],  # query, path, header, cookie
             'required': parameter.get('required', 0) == 1,
             'description': parameter['description'] or '',
             'schema': convert_type(parameter['dataType'], model_refs)
-            }
+        }
+
 
 def convert_body(params, model_refs):
     schemas = {
@@ -84,7 +90,8 @@ def convert_body(params, model_refs):
                 raise Exception("There can be only one unamed body param")
             return {'$ref': gen_model_path(param['dataType'])}
         schemas['properties'][param['name']] = convert_type(param['dataType'], model_refs)
-    return schemas            
+    return schemas
+
 
 def convert_operation(operations, model_refs):
     # cat schema/apis-me.json | jq '.apis[].operations[].parameters[] | select(.paramType == "body")' |less
@@ -121,9 +128,11 @@ def convert_operation(operations, model_refs):
             }
         yield (operation['httpMethod'].lower(), method)
 
+
 def convert_api_paths(paths, model_refs):
     for path in paths:
         yield (path['path'], dict(convert_operation(path['operations'], model_refs)))
+
 
 def convert_api_to_oa3(api_json):
     model_refs = list(api_json['models'].keys())
@@ -141,22 +150,3 @@ def convert_api_to_oa3(api_json):
             'schemas': dict(convert_model(api_json['models'], model_refs))
         }
     }
-
-# apis = {
-#     'me': json.load(open(Path('schema') / 'apis-me.json', 'r')),
-#     'ip': json.load(open(Path('schema') / 'apis-ip.json', 'r')),
-#     'dedicatedCloud': json.load(open(Path('schema') / 'apis-dedicatedCloud.json', 'r')),
-#     }
-
-# if __name__ == '__main__':
-#     apis = requests.get('https://api.ovh.com/1.0/').json()
-#     api_base = apis['basePath']
-#     for api in apis['apis']:
-#         api_path = '{}{}.json'.format(api_base, api['path'])
-#         api_name = api['path'].strip('/').replace('/', '_')
-#         print(api_path)
-#         print(api_name)
-#         api = requests.get(api_path).json()
-#         json.dump(api, open(Path('schemas') / 'ovh' / 'apis-{}.json'.format(api_name), 'w'))
-#         api_swagger = convert_api_to_oa3(api)
-#         yaml.dump(api_swagger, open(Path('schemas') / 'oa3' / 'apis-{}.yml'.format(api_name), 'w'), allow_unicode=True, default_flow_style=False)
