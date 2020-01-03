5b2bb1b8d10014d6b2803a275807328f41415ec6
Pierre Gronlier <pierre.gronlier@corp.ovh.com>
2020-01-03 12:44:40 +0000

    listen on all interface

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from service import main
 
 if __name__ == "__main__":
     app = main.make_app(autoreload=True)
-    app.listen(8000)
+    app.listen(8000, address="0.0.0.0")
     tornado.ioloop.IOLoop.current().start()
